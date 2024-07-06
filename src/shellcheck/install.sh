d67169c5c1f1eccee0e593348cd492f74fdf04f9
0x4448 <86135470+0x4448@users.noreply.github.com>
2024-07-06 17:30:41 +0000

    refactor: shellcheck install script

--- src/shellcheck/install.sh
+++ src/shellcheck/install.sh
@@ -1,29 +1,47 @@
 #!/usr/bin/env bash
 set -eu
 
+
+# Feature Options
 VERSION=${VERSION:-"stable"}
 HASH=${HASH:-"none"}
 
-# Package requirements
-apt update
-apt install --yes --no-install-recommends \
-  ca-certificates \
-  curl \
-  xz-utils
 
-tempDir=$(mktemp -d)
-pushd "$tempDir" > /dev/null
+# Script Variables
+fileName="shellcheck.tar.xz"
 
-url="https://github.com/koalaman/shellcheck/releases/download/$VERSION/shellcheck-$VERSION.linux.$(uname -m).tar.xz"
-output="shellcheck.tar.xz"
-curl -fsSL -o "$output" "$url"
 
-if [ "$HASH" != "none" ]; then
-  echo "$HASH $output" | sha256sum --check
-fi
+# Functions
+install_requirements() {
+  apt update
+  apt install --yes --no-install-recommends \
+    ca-certificates \
+    curl \
+    xz-utils
+}
 
-tar xf "$output"
-install "shellcheck-$VERSION/shellcheck" /usr/local/bin/shellcheck
+initialize_tempdir() {
+  tempDir=$(mktemp -d)
+  cd "$tempDir" || exit 1
+  trap 'rm -rf "$tempDir"' EXIT
+}
 
-popd > /dev/null
-rm -rf "$tempDir"
+download() {
+  url="https://github.com/koalaman/shellcheck/releases/download/$VERSION/shellcheck-$VERSION.linux.$(uname -m).tar.xz"
+  curl -fsSL -o "$fileName" "$url"
+  if [ "$HASH" != "none" ]; then
+    echo "$HASH $fileName" | sha256sum --check
+  fi
+}
+
+install_feature() {
+  tar xf "$fileName"
+  install "shellcheck-$VERSION/shellcheck" /usr/local/bin/shellcheck
+}
+
+
+# Main
+install_requirements
+initialize_tempdir
+download
+install_feature
